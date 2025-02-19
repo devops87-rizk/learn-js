1456462b282a5067e3eda3ad7a5b3fa77209ddca
Rizky Rahman <rizk87devops@gmail.com>
2025-02-20 06:06:40 +0800

    if statement

---
+++ if-statement.js
@@ -0,0 +1,16 @@
+// console.log('sebelum pembanding');
+// if (1 + 1 === 2) {
+//     console.log('di dalam pembanding');
+// }
+// console.log('setelah pembanding')
+
+let number = Math.random();
+console.log(number);
+
+if (number >= 0.5) {
+    console.log('Angka lebih besar dari 0.5');
+}
+
+if (number <= 0.5) {
+    console.log('Angka lebih kecil dari 0.5');
+}
