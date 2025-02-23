0d877af5bcf6dff2775592c137e5c88690d45ebe
Rizky Rahman <rizk87devops@gmail.com>
2025-02-23 10:44:49 +0800

    else if statment

--- if-statement.js
+++ if-statement.js
@@ -4,13 +4,23 @@
 // }
 // console.log('setelah pembanding')
 
-let number = Math.random();
-console.log(number);
+// let number = Math.random();
+// console.log(number);
 
-if (number >= 0.5) {
-    console.log('Angka lebih besar dari 0.5');
-}
+// if (number >= 0.5) {
+//     console.log('Angka lebih besar dari 0.5');
+// }
 
-if (number <= 0.5) {
-    console.log('Angka lebih kecil dari 0.5');
+// if (number <= 0.5) {
+//     console.log('Angka lebih kecil dari 0.5');
+// }
+
+let age = 8;
+
+if ( age === 18 ) {
+    console.log("Boleh masuk.")
+}   else if (age === 15) {
+    console.log("Perlu dibimbing Orang tua")
+}   else if (age === 8) {
+    console.log("Udah pergi saja.")
 }
