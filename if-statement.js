1350aacfe2e1c4eb5d9c8bb20b55c1b4e09b3a3a
Rizky Rahman <rizk87devops@gmail.com>
2025-02-23 11:41:23 +0800

    else

--- if-statement.js
+++ if-statement.js
@@ -15,12 +15,28 @@
 //     console.log('Angka lebih kecil dari 0.5');
 // }
 
-let age = 8;
+const age = prompt('Isi rentang usia 15 - 18 thn');
 
-if ( age === 18 ) {
-    console.log("Boleh masuk.")
-}   else if (age === 15) {
-    console.log("Perlu dibimbing Orang tua")
-}   else if (age === 8) {
-    console.log("Udah pergi saja.")
+if (age === '18') {
+    console.log("masuk saja")
+}   else if (age === '17') {
+    console.log("butuh dampingan")
+}   else if (age === '16') {
+    console.log("butuh dampingan")
+}   else if (age === '15') {
+    console.log("butuh dampingan")
+}   else {
+    console.log("jangan masuk")
 }
+
+
+
+// let age = 8;
+
+// if ( age === 18 ) {
+//     console.log("Boleh masuk.")
+// }   else if (age === 15) {
+//     console.log("Perlu dibimbing Orang tua")
+// }   else if (age === 8) {
+//     console.log("Udah pergi saja.")
+// }
