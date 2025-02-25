9a85c9adc873bed82a476c25ef3c732e3ba3b27a
Rizky Rahman <rizk87devops@gmail.com>
2025-02-25 23:08:00 +0800

    learn array

---
+++ array.js
@@ -0,0 +1,24 @@
+// Array with string collection
+let color = ['Red', 'Blue', 'Green', 'Yellow']
+
+// Change Array
+color[0] = 'black'
+
+// Add Array
+color[4] = 'Red'
+color[5] = 'Purple'
+color[6] = 'Pink'
+
+// Push
+color.push('Tosca')
+
+// Unshift
+color.unshift('Silver')
+
+// Pop
+color.pop()
+
+// Shift
+color.shift()
+
+console.log(color)
