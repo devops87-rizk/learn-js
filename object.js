80ac138ebc0155feed336a187ec52c0e49f11925
Rizky Rahman <rizk87devops@gmail.com>
2025-03-01 23:18:32 +0800

    Object data structure

--- object.js
+++ object.js
@@ -1,9 +1,40 @@
 //Object Data Structure
-const people = {
-    name: 'Ali',
-    tanggal: 16,
-    alamat: 'Cirebon'
-}
+// const people = {
+//     name: 'Ali',
+//     tanggal: 16,
+//     alamat: 'Cirebon'
+// }
 
-console.log(people)
+// console.log(people)
 
+//Nested Object
+
+// const shoes = {
+//     type: 'Foot Wear',
+//     price: '1.000.000',
+//     isReady: true,
+//     size: [37, 38, 39, 40, 41],
+//     location: {
+//         indonesia: 48,
+//         singapore: 50
+//     }
+// }
+
+// console.log(shoes)
+
+//Nested Array + Object
+
+const shoppingChart = [
+    {
+        product: 'Disposible',
+        price: '1.200',
+        quantity: 3
+    },
+    {
+        product: 'Infusion Set',
+        price: '2.500',
+        quantity: 5
+    }
+]
+
+console.log(shoppingChart)
