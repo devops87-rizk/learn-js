1d367564281074ba04873888dec2167be6d52978
Rizky Rahman <rizk87devops@gmail.com>
2025-03-12 05:18:48 +0800

    Done break

---
+++ break.js
@@ -0,0 +1,16 @@
+// Break  while
+
+// let input = prompt('Hei, say something!');
+// while (true) {
+//     input = prompt(input);
+//     if (input === 'stop') break;
+// }
+
+// alert('ok');
+
+// Break if
+
+for (let i = 0; i < 10000; i++) {
+    console.log(i);
+    if (i === 100) break;
+}
